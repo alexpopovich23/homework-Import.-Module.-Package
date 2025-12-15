27390ebb7e50ed803a53fe227d5b982a48594214
Mbapesh <darkstorm2010@mail.ru>
2025-12-15 12:57:08 +0300

    initial commit

---
+++ dirty_main.py
@@ -0,0 +1,8 @@
+from application.salary import *
+from application.db.people import *
+
+if __name__ == '__main__':
+    from datetime import datetime
+    print(f"Текущая дата: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    calculate_salary()
+    get_employees()
---
+++ main.py
@@ -0,0 +1,11 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    
+    current_date = datetime.datetime.now()
+    print(f"Текущая дата и время: {current_date}")
+    print("Программа выполнена успешно!")
